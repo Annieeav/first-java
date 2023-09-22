82397e604d4a8677a689993141efeb8e7e71fd07
Annie <ana_niagu@ymail.com>
2023-09-22 12:17:57 +0200

    Person e App

---
+++ App.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("scrivi un numero");
+        int a = sc.nextInt();
+        sc.close();
+        System.out.println(a);
+    }
+
+}
---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    private String name;
+    private String surname;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
